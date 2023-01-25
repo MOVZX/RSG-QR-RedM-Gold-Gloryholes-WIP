32f6c511009e051b59ca95dd52270f1fee6cb5de
Uncle Zeke <117952305+MDGamesmaster@users.noreply.github.com>
2023-01-25 15:15:59 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,23 @@
+Config = {}
+
+-- Locations of gold mining gloryholes
+Config.Gloryholes = {
+    {x = -622.36, y = -230.48, z = 38.05},
+    {x = -618.57, y = -232.28, z = 38.05},
+    {x = -614.82, y = -234.05, z = 38.05}
+}
+
+-- Time to pan for gold (in seconds)
+Config.GoldpanTime = 20
+
+-- Chance of finding extra items (out of 100)
+Config.ExtraItemChance = 10
+
+-- Extra items that can be found
+Config.ExtraItems = {
+    {name = "diamond", count = 1},
+    {name = "emerald", count = 1},
+    {name = "ruby", count = 1},
+    {name = "sapphire", count = 1},
+    {name = "amethyst", count = 1}
+}
