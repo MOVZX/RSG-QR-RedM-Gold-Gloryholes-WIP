7f140c2c25b8963572af350da781fb3cfa294996
Uncle Zeke <117952305+MDGamesmaster@users.noreply.github.com>
2023-01-25 15:11:55 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,105 +1,68 @@
-local config = require("config") -- import the config file
+local goldpan = false
+local goldpanBlip
+local goldpanTimer
 
--- Function to check if the player is crouched
-function isCrouched()
-    local player = GetPlayerPed(-1)
-    return IsPedCrouching(player)
-end
-
--- Function to check if the player is in a valid water location
-function isValidWaterLocation()
-    local player = GetPlayerPed(-1)
-    local playerPos = GetEntityCoords(player)
-    -- check if player is in a valid water location
-    -- you should replace this with your own check
-    return isWater(playerPos.x, playerPos.y, playerPos.z)
-end
-
--- Function to check if another player is within 10m
-function isPlayerNear()
-    local player = GetPlayerPed(-1)
-    local playerPos = GetEntityCoords(player)
-    local players = GetPlayers()
-    for _, playerId in ipairs(players) do
-        if playerId ~= PlayerId() then
-            local otherPlayer = GetPlayerPed(playerId)
-            local otherPlayerPos = GetEntityCoords(otherPlayer)
-            local distance = Vdist(playerPos.x, playerPos.y, playerPos.z, otherPlayerPos.x, otherPlayerPos.y, otherPlayerPos.z)
-            if distance <= 10.0 then
-                return true
-            end
-        end
-    end
-    return false
-end
-
--- Function to start the gold panning animation
-function startPanningAnimation()
-    local player = GetPlayerPed(-1)
-    RequestAnimDict("animation")
-    while not HasAnimDictLoaded("animation") do
-        Citizen.Wait(0)
-    end
-    TaskPlayAnim(player, "animation", "gold_panning", 8.0, -8, -1, 49, 0, 0, 0, 0)
-    Citizen.Wait(config.panningTime)
-    ClearPedTasks(player)
-end
-
--- Function to get the random item
-function getRandomItem()
-    local totalWeight = 0
-    local itemList = {}
-    for item, weight in pairs(config.items) do
-        totalWeight = totalWeight + weight
-        itemList[totalWeight] = item
-    end
-
-    local randomWeight = math.random(1, totalWeight)
-    for weight, item in pairs(itemList) do
-        if weight >= randomWeight then
-            return item
-        end
-    end
-end
-
--- Function to handle the gold panning event
-function goldPanningEvent()
-    if isCrouched() and isValidWaterLocation() and not isPlayerNear() then
-        startPanningAnimation()
-        local item = getRandomItem()
-        -- do something with the obtained item
-    else
-        if not isCrouched() then
-            -- show notification to player that they need to crouch
-        elseif not isValidWaterLocation() then
-            -- show notification that the location is not valid
-        else
-        -- show notification that another player is too close
-    end
-end
-
--- Function to check if player needs to move
-function checkMovement()
-    -- check if player has found between 5 and 15 items
-    -- check if player has moved at least 10m away
-    -- if they haven't, show a notification and prevent them from panning
-end
-
--- Function to check if player is in a gloryhole location
-function checkGloryhole()
-    -- check if player is in a gloryhole location
-    -- if they are, show a notification and increase their chances of finding more than 1 item
-end
-
--- Register the gold panning event to be triggered when the player uses the goldpan item
-RegisterNetEvent("goldpan:use")
-AddEventHandler("goldpan:use", goldPanningEvent)
-
--- Register the check movement event to be triggered periodically
 Citizen.CreateThread(function()
     while true do
-        checkMovement()
-        checkGloryhole()
-        Citizen.Wait(1000)
+        Citizen.Wait(0)
+        local player = PlayerPedId()
+        local playerCoords = GetEntityCoords(player)
+
+        -- Check if player is in a designated gloryhole
+        for i,v in ipairs(Config.Gloryholes) do
+            if(Vdist(playerCoords.x, playerCoords.y, playerCoords.z, v.x, v.y, v.z) < 5.0)then
+                if goldpan then
+                    -- Stop panning
+                    goldpan = false
+                    ClearPedTasks(player)
+                    goldpanBlip = nil
+                    goldpanTimer = nil
+                else
+                    -- Check if player has goldpan
+                    local hasGoldpan = false
+                    TriggerServerEvent("qrp_checkInventory", "goldpan")
+                    Citizen.Wait(2000)
+                    if hasGoldpan then
+                        goldpan = true
+                        goldpanBlip = AddBlipForCoord(v.x, v.y, v.z)
+                        SetBlipSprite(goldpanBlip,  501)
+                        SetBlipColour(goldpanBlip,  2)
+                        SetBlipAsShortRange(goldpanBlip,  true)
+                        BeginTextCommandSetBlipName("STRING")
+                        AddTextComponentString("Gold Mining Gloryhole")
+                        EndTextCommandSetBlipName(goldpanBlip)
+                        TaskStartScenarioInPlace(player, "WORLD_HUMAN_GOLD_PAN_ARMED", 0, true)
+                        goldpanTimer = Config.GoldpanTime
+                    else
+                        exports['mythic_notify']:SendAlert('error', 'You dont have a goldpan')
+                    end
+                end
+            end
+        end
+
+        if goldpan then
+            -- Panning animation
+            TaskPlayAnim(player, "anim@arena@celeb@flat@paired@no_props@", "pan_loop", 8.0, -8.0, -1, 1, 0, false, false, false)
+            if goldpanTimer <= 0 then
+                -- End panning
+                goldpan = false
+                ClearPedTasks(player)
+                goldpanBlip = nil
+                goldpanTimer = nil
+                exports['mythic_notify']:SendAlert('inform', 'You found some gold')
+                -- Add gold to inventory
+                TriggerServerEvent("qrp_addInventoryItem", "gold", 1)
+                -- Update chance of finding extra items
+                local chance = math.random(1, 100)
+                if chance <= Config.ExtraItemChance then
+                    local item = Config.ExtraItems[math.random(1, #Config.ExtraItems)]
+                    TriggerServerEvent("qrp_addInventoryItem", item.name, item.count)
+                    exports['mythic_notify']:SendAlert('inform', 'You also found '..item.count..' '..item.name)
+                end
+            else
+                goldpanTimer - 1
+                Citizen.Wait(1000)
+            end
+        end
     end
 end)
